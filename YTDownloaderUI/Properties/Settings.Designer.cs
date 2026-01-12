a4e90fce25a2db60331bf6cd20f283e9ce29a0de
Kyler Riggs <100742516+ky-ler@users.noreply.github.com>
2026-01-12 12:49:24 -0500

    feat: add user setting for custom download directory

--- YTDownloaderUI/Properties/Settings.Designer.cs
+++ YTDownloaderUI/Properties/Settings.Designer.cs
@@ -94,5 +94,17 @@ namespace YTDownloaderUI.Properties {
                 this["FFprobeLocation"] = value;
             }
         }
+
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string DownloadDirectory {
+            get {
+                return ((string)(this["DownloadDirectory"]));
+            }
+            set {
+                this["DownloadDirectory"] = value;
+            }
+        }
     }
 }
