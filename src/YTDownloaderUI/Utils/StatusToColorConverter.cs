a403fc12880fb6d25f14a43d83e6f86c822189ae
Kyler Riggs <100742516+ky-ler@users.noreply.github.com>
2026-01-29 10:30:27 -0500

    feat: add StatusToColorConverter for visual status indicators

---
+++ src/YTDownloaderUI/Utils/StatusToColorConverter.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace YTDownloaderUI.Utils;
+
+public class StatusToColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var status = value as string;
+        return status switch
+        {
+            "Error" or "Failed" => Brushes.Red,
+            "Finished" => Brushes.Green,
+            "Cancelled" => Brushes.Gray,
+            _ => Application.Current.Resources["TextFillColorPrimaryBrush"] ?? Brushes.White
+        };
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
